f01beff986cbdb1d50c807f0478ce0c701232706
Leandro Raul Batista Silva <1800714@SALAS.AULAS>
2018-08-21 21:46:04 -0300

    funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -2,13 +2,13 @@
 def bemvindo():
 	print("Bem Vindo a Agenda")
 	print("Selecione uma Opcao")
-	# print("1  Adicionar um novo contato")
-	# print("2  Listar os contatos da agenda")
+	print("1  Adicionar um novo contato")
+	print("2  Listar os contatos da agenda")
 
 #Funcoes do processo
 def adicionar():
 	print("Adicionar um registro")
-	# agenda = open("agendatelefonica.csv",'a')
+	#agenda = open("agendatelefonica.csv",'a')
 	# nome = input("Nome do Contato:")
 	# telefone = input("Digite o telefone:")
 	# print("Contato salvo com nome:",nome," e numero",telefone)
