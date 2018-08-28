b3adc7ab0d28745f421977c5d507997e041d1d20
Leandro Raul Batista Silva <1800714@salas.aulas>
2018-08-27 21:09:14 -0300

    funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -8,26 +8,26 @@ def bemvindo():
 #Funcoes do processo
 def adicionar():
 	print("Adicionar um registro")
-	#agenda = open("agendatelefonica.csv",'a')
-	# nome = input("Nome do Contato:")
-	# telefone = input("Digite o telefone:")
-	# print("Contato salvo com nome:",nome," e numero",telefone)
-	# agenda.write(nome)
-	# agenda.write(",")
-	# agenda.write(telefone)
-	# agenda.write(",")
-	# agenda.write("\n")
-	# agenda.close()
+	agenda = open("agendatelefonica.csv",'a')
+	nome = input("Nome do Contato:")
+	telefone = input("Digite o telefone:")
+	print("Contato salvo com nome:",nome," e numero",telefone)
+	agenda.write(nome)
+	agenda.write(",")
+	agenda.write(telefone)
+	agenda.write(",")
+	agenda.write("\n")
+	agenda.close()
 	
 def listar():
-	print("Lista de Contatos")
-	# agenda = open("agendatelefonica.csv")
-	# numero = 0
-	# while numero < 25:
-	# 	print (agenda.readline())
-	# 	numero = numero + 1
-	# print("Listado correctamente")	
-	# agenda.close()
+      print("Lista de Contatos")
+      agenda = open("agendatelefonica.csv")
+      numero = 0
+      while numero < 25:
+           print(agenda.readline())
+           numero = numero + 1
+      print("Listado correctamente")
+      agenda.close()
 
 def falha():
-	print("Opcao Incorreta")
+      print("Opcao Incorreta")
